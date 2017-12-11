5e31dd3fa9f9654ed5b958c75519267b74def6cb
Paul Putier <paul.putier@epita.fr>
2017-12-11 19:02:42 +0100

    tout supprimé

--- personal_math_functions.h
+++
@@ -1,9 +0,0 @@
-#ifndef _PERSONAL_MATH_FUNCTIONS_H_
-#define _PERSONAL_MATH_FUNCTIONS_H_
-
-double Exp(double x);
-double* Han(double *A, double *B, size_t length, double *new_matrice);
-double Sigmoid(double z);
-double Sigmoid_prime(double z);
-
-#endif
